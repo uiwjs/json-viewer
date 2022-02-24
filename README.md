d2cbe3b10e83d0926c97ceee2c59376b55e97a27
jaywcjlove <398188662@qq.com>
2022-02-24 19:04:08 +0800

    feat: add url parameters.

--- README.md
+++ README.md
@@ -6,6 +6,21 @@ https://uiwjs.github.io/json-viewer
 
 [![Online JSON Viewer](https://user-images.githubusercontent.com/1680273/149053681-1f9f525e-9b45-45f8-be3c-34c1ac91fc51.png)](https://uiwjs.github.io/json-viewer)
 
+**url parameters:**
+
+```bash
+# Hiden Header
+https://uiwjs.github.io/json-viewer/#/?hidenheader=1
+# Hiden GitHub Corners
+https://uiwjs.github.io/json-viewer/#/?corner=0
+# GitHub corners href
+https://uiwjs.github.io/json-viewer/#/?corner=1&cornerhref=cornerhref=https://github.com/uiwjs/json-viewer
+# view=preview|editor
+https://uiwjs.github.io/json-viewer/#/?view=preview
+# Share Code
+https://uiwjs.github.io/json-viewer/#/?hidenheader=1&json={%20%20"a":%20234}
+```
+
 **Reference:**
 
 https://www.json.org/json-en.html  
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,49 @@
-import React, { useCallback, useEffect, useRef} from 'react';
+import React, { Fragment, useCallback, useEffect, useRef} from 'react';
 import Split from '@uiw/react-split';
 import GitHubCorners from '@uiw/react-github-corners';
 import JsonViewer from 'react-json-view';
 import CodeMirror, { ReactCodeMirrorRef } from '@uiw/react-codemirror';
 import { json as jsonLang } from '@codemirror/lang-json';
+import { createHashHistory } from 'history';
 import styles from './App.module.css';
 
+type Parameters = {
+  json?: string;
+  cornerhref?: string;
+  hidenheader?: '1' | '0';
+  corner?: '1' | '0';
+  view?: 'preview'| 'editor';
+}
+const history = createHashHistory();
+const getURLParameters = (url: string): Parameters =>
+  (url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce(
+    (a: any, v) => (
+      ((a[v.slice(0, v.indexOf('='))] = v.slice(v.indexOf('=') + 1)), a)
+    ),
+    {}
+  );
+const objectToQueryString = (queryParameters: Parameters) => {
+  return queryParameters
+    ? Object.entries(queryParameters).reduce(
+        (queryString, [key, val], index) => {
+          const symbol = queryString.length === 0 ? '?' : '&';
+          queryString +=
+            typeof val === 'string' ? `${symbol}${key}=${val}` : '';
+          return queryString;
+        },
+        ''
+      )
+    : '';
+};
+
 const App = () => {
-  const lineRef = useRef<HTMLSpanElement>(null);
+  const param = getURLParameters(window.location.href);
   const cmRef = useRef<ReactCodeMirrorRef>(null);
-  const [code, setCode] = React.useState('');
+  param.json = param.json ? decodeURI(param.json): undefined;
+  const [code, setCode] = React.useState(decodeURIComponent(param.json || ''));
   const [json, setJson] = React.useState();
   const [message, setMessage] = React.useState('');
+  const [linebar, setLinebar] = React.useState('');
   
   const handleJson = useCallback(() => {
     setMessage('');
@@ -48,71 +80,102 @@ const App = () => {
     }
   }, [code]);
 
+  const shareJson = () => {
+    param.json = encodeURI(code);
+    history.push(`${window.location.pathname}${objectToQueryString(param)}`, { some: "state" });
+  }
+
   useEffect(() => {
     handleJson()
   }, [code, handleJson]);
 
+  const editor = (
+    <div style={{ minWidth: 230, width: param.view === 'editor' ? '100%' : '45%', position: 'relative', backgroundColor: 'rgb(245, 245, 245)' }}>
+      <div style={{overflow: 'auto',height: '100%', boxSizing: 'border-box' }}>
+        <CodeMirror
+          value={code}
+          ref={cmRef}
+          height="100%"
+          style={{ height: '100%' }}
+          extensions={[jsonLang()]}
+          onUpdate={(cm) => {
+            if (param.hidenheader === '1') {
+              return;
+            }
+            const { selection } = cm.state;
+            const line = cm.view.state.doc.lineAt(selection.main.from);
+            setLinebar(`Line ${line.number}/${cm.state.doc.lines}, Column ${cm.state.selection.main.head - line.from + 1}`);
+            const text = cm.state.sliceDoc(selection.main.from, selection.main.to);
+            if (text) {
+              if (selection.ranges.length > 1) {
+                setLinebar(`${selection.ranges.length} selection regions`);
+              } else {
+                setLinebar(`${text.split('\n').length} lines, ${text.length} characters selected`);
+              }
+            }
+          }}
+          onChange={(value, viewUpdate) => {
+            setCode(value)
+          }}
+        />
+      </div>
+    </div>
+  );
+
+  const preview = (
+    <div style={{ flex: 1, minWidth: 230, userSelect: 'none', padding: 10, overflow: 'auto' }}>
+      {message && (
+        <pre style={{ padding: 0, margin: 0, color: 'red' }}>
+          {message}
+        </pre>
+      )}
+      {json && typeof json == 'object' && (
+        <JsonViewer src={json!} theme="rjv-default" style={{  }} displayDataTypes={false} />
+      )}
+    </div>
+  );
   return (
     <div className={styles.app}>
-      <GitHubCorners fixed zIndex={999} size={43} target="__blank" href="https://github.com/uiwjs/json-viewer" />
+      {!Number(param.corner) && (
+        <GitHubCorners fixed zIndex={999} size={43} target="__blank" href={param.cornerhref ? param.cornerhref : 'https://github.com/uiwjs/json-viewer'} />
+      )}
       <Split mode="vertical" visiable={false}>
-        <div className={styles.header} style={{  }}>
-          <h1>JSON Viewer</h1>
-          <div className={styles.toolbar}>
-            <div>
-              <span ref={lineRef} />
-            </div>
-            {message && (
-              <div className={styles.message}>{message}</div>
-            )}
-            <div className={styles.btn}>
-              <button onClick={formatJson}>
-                Format
-              </button>
-              <button onClick={() => formatJson(null, 0)}>
-                Compress
-              </button>
-            </div>
-          </div>
-        </div>
-        <Split style={{ flex: 1, height: 'calc(100% - 32px)' }}>
-          <div style={{ minWidth: 230, width: '45%', position: 'relative', backgroundColor: 'rgb(245, 245, 245)' }}>
-            <div style={{overflow: 'auto',height: '100%', boxSizing: 'border-box' }}>
-              <CodeMirror
-                value={code}
-                ref={cmRef}
-                height="100%"
-                style={{ height: '100%' }}
-                extensions={[jsonLang()]}
-                onUpdate={(cm) => {
-                  const { selection } = cm.state;
-                  const line = cm.view.state.doc.lineAt(selection.main.from);
-                  lineRef.current!.innerHTML = `Line ${line.number}/${cm.state.doc.lines}, Column ${cm.state.selection.main.head - line.from + 1}`;
-                  const text = cm.state.sliceDoc(selection.main.from, selection.main.to);
-                  if (text) {
-                    if (selection.ranges.length > 1) {
-                      lineRef.current!.innerHTML = `${selection.ranges.length} selection regions`;
-                    } else {
-                      lineRef.current!.innerHTML = `${text.split('\n').length} lines, ${text.length} characters selected`;
-                    }
-                  }
-                }}
-                onChange={(value, viewUpdate) => {
-                  setCode(value)
-                }}
-              />
+        {param.hidenheader !== '1' && (
+          <div className={styles.header} style={{  }}>
+            <h1>JSON Viewer</h1>
+            <div className={styles.toolbar}>
+              <div>
+                {linebar && (
+                  <span> {linebar} </span>
+                )}
+              </div>
+              {message && (
+                <div className={styles.message}>{message}</div>
+              )}
+              <div className={styles.btn}>
+                <button onClick={formatJson}>
+                  Format
+                </button>
+                <button onClick={() => formatJson(null, 0)}>
+                  Compress
+                </button>
+                {code && (
+                  <button onClick={() => shareJson()}>
+                    Share
+                  </button>
+                )}
+              </div>
             </div>
           </div>
-          <div style={{ flex: 1, minWidth: 230, userSelect: 'none', padding: 10, overflow: 'auto' }}>
-            {message && (
-              <pre style={{ padding: 0, margin: 0 }}>
-                {message}
-              </pre>
-            )}
-            {json && typeof json == 'object' && (
-              <JsonViewer src={json!} theme="rjv-default" style={{  }} displayDataTypes={false} />
-            )}
-          </div>
+        )}
+        <Split style={{ flex: 1, height: param.hidenheader !== '1' ? 'calc(100% - 32px)' : '100%' }}>
+          {!param.view && (
+            <Fragment>
+              {editor}{preview}
+            </Fragment>
+          )}
+          {param.view === 'editor' && editor}
+          {param.view === 'preview' && preview}
         </Split>
       </Split>
     </div>
